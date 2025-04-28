669a77a3eaabb5995e79920eab0471abc8063771
i.bennaji.ext <i.bennaji.ext@lesaffre.com>
2025-04-28 10:45:26 +0100

    update CV

--- components/ActionButtons.tsx
+++ components/ActionButtons.tsx
@@ -21,7 +21,7 @@ const ActionButtons = () => (
       </motion.button>
     </Link>
     <Link 
-      href="https://drive.google.com/file/d/1NdbStN4ufDhZy7-lKyKLAFWTlIwMBEga/view?usp=sharing"
+      href="https://drive.google.com/file/d/1Yc17UvTHfjwr4eB0Wcj1py_fp5HLhCi3/view?usp=sharing"
       target="_blank" 
       rel="noopener noreferrer"
     >
